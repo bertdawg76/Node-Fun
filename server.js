2f314a64fb287f2fef9eb7b192de5858a82b1397
bertcoleman76@gmail.com <bertcoleman76@gmail.com>
2015-08-10 13:24:25 -0600

    starting with twilio

--- server.js
+++ server.js
@@ -5,15 +5,51 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+var cors = require('cors');
+
+var request = require('request');
+
+var twilio = require('twilio');
+
 var app = express();
 
 var port = 4000;
 
+var message = {
+	"message": "hello world"
+};
 
-//app.listen(port, function(){
-//	console.log('Im watching you port 4000')
-//});
+//Middleware
 
-app.listen(process.env.PORT, process.env.IP);
-console.log('powering on');
+app.use(express.static('public'));
+
+//app.use(bodyParser());
+
+app.use(cors());
+
+//for deprecated error, if not use app.use(bodyParser());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+//endpoints
+
+app.get('/api/message', function(req, res) {
+	return res.json(message);
+
+	
+});
+
+app.post('/api/send_text_message', function(req, res){
+	console.log(req.body);
+	res.send();
+})
+
+app.listen(port, function(){
+ console.log('Im watching you port 4000')
+});
+
+//app.listen(process.env.PORT, process.env.IP);
+//console.log('powering on');
 
